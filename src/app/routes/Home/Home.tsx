65a39c72e0a235fa65dcb32c6ad29565da48759b
Damian Nowakowski <dev@proto.zone>
2018-12-17 20:08:39 +0100

    styled-components and react update

--- src/app/routes/Home/Home.tsx
+++ src/app/routes/Home/Home.tsx
@@ -1,13 +1,23 @@
 import * as React from 'react'
 import { connect } from 'react-redux'
 import { IStoreState } from 'src/store/types';
+import styled from 'styled-components'
 
 interface HomeProps { }
+
+const Dupa = styled.div`
+width: 100px;
+height: 100px;
+background: red;
+`
 
 class Home extends React.Component<HomeProps> {
   render() {
     return (
-      <div>Routing works good</div>
+      <div>
+        <Dupa />
+        <div>Routing works good</div>
+      </div>
     )
   }
 }
